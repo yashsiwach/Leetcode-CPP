f794c02d4da150621efc6e1ed2816453bfa0c470
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-06-06 22:08:13 +0530

    Time: 4 ms (74.54%) | Memory: 9.1 MB (66.22%) - LeetSync

---
+++ can-make-arithmetic-progression-from-sequence/can-make-arithmetic-progression-from-sequence.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canMakeArithmeticProgression(vector<int>& arr) {
+        sort(arr.begin(),arr.end());
+        int diff=arr[0]-arr[1];
+        cout<<diff<<" ";
+
+        for(int i=0;i<arr.size()-1;i++)
+        {
+            if(arr[i]-arr[i+1]!=diff) return false;
+
+        }return true ;
+        
+    }
+};

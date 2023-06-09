ad684771e7b8ccc6981bfa5d7efaa4e81aa868f6
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-06-09 11:26:46 +0530

    Time: 3 ms (68.45%) | Memory: 6.1 MB (25.04%) - LeetSync

--- fibonacci-number/fibonacci-number.cpp
+++ fibonacci-number/fibonacci-number.cpp
@@ -1,21 +1,39 @@
 class Solution {
 public:
-    int solve(int n,vector<int>&jat)
-    {
-        if(n==0||n==1)
+    //Top down (recursion + Memorization)
+    // int solve(int n,vector<int>&jat)
+    // {
+    //     if(n==0||n==1)
+    //     {
+    //         return n;
+    //     }
+    //     if(jat[n]!=-1)
+    //     {
+    //         return jat[n];
+    //     }
+    //     jat[n]=solve(n-1,jat)+solve(n-2,jat);
+    //     return jat[n];
+    // }
+
+    int bottomup(int n){
+        vector<int>dp(n+1,-1);
+        dp[0]=0;
+        
+        if(n==0) return dp[0];
+        dp[1]=1;
+        if(n==1) return dp[1];
+        
+        for(int i=2;i<=n;i++)
         {
-            return n;
+            dp[i]=dp[i-1]+dp[i-2];
         }
-        if(jat[n]!=-1)
-        {
-            return jat[n];
-        }
-        jat[n]=solve(n-1,jat)+solve(n-2,jat);
-        return jat[n];
+        return dp[n];
     }
     int fib(int n) {
-    vector<int>jat(n+1,-1);
-    int ad=solve(n,jat);
-    return ad;
+    //vector<int>jat(n+1,-1);
+    //int ad=solve(n,jat);
+    //return ad;
+    return bottomup(n);
+
     }
 };

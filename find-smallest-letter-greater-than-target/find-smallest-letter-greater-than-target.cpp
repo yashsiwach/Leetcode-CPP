5c570ae53882c8b9d0a82d2606f6aae1c365f0b8
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-06-09 10:08:51 +0530

    Time: 13 ms (84.16%) | Memory: 15.9 MB (72.43%) - LeetSync

--- find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
+++ find-smallest-letter-greater-than-target/find-smallest-letter-greater-than-target.cpp
@@ -1,23 +1,24 @@
 class Solution {
 public:
     char nextGreatestLetter(vector<char>& letters, char target) {
-        set<int>jat;
-        if(letters[letters.size()-1]<=target)
-        {
-            return letters[0];
-        }
-        for(int i=0;i<letters.size();i++)
-        {
-            if(letters[i]>target){
-            int temp=letters[i]-target;
-            if(temp==0)
-            {
-                return letters[i+1];
-            }
-            jat.insert(temp);}
-        }
-        return target+*jat.begin();
-
+        // set<int>jat;
+        // if(letters[letters.size()-1]<=target)
+        // {
+        //     return letters[0];
+        // }
+        // for(int i=0;i<letters.size();i++)
+        // {
+        //     if(letters[i]>target){
+        //     int temp=letters[i]-target;
+        //     if(temp==0)
+        //     {
+        //         return letters[i+1];
+        //     }
+        //     jat.insert(temp);}
+        // }
+        // return target+*jat.begin();
+        int i=upper_bound(letters.begin(), letters.end(), target)-letters.begin();
+        return (i==letters.size())?letters[0]:letters[i];
 
 
         

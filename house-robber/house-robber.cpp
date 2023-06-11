00465cf8e5d3e04e6aaa1a1cce996ff01e59816b
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-06-12 01:53:16 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (21.54%) - LeetSync

--- house-robber/house-robber.cpp
+++ house-robber/house-robber.cpp
@@ -1,29 +1,48 @@
 class Solution {
 public:
-    int solvedp(vector<int>&nums,int n,vector<int>&dp)
+    // int solvedp(vector<int>&nums,int n,vector<int>&dp)
+    // {
+    //     if(n<0)
+    //     {
+    //         return 0;
+    //     }
+    //     if(n==0)
+    //     {
+    //         return nums[0];
+    //     }
+    //     if(dp[n]!= -1)
+    //     {
+    //         return dp[n];
+    //     }
+    //     int in=solvedp(nums,n-2,dp)+nums[n];
+    //     int ex=solvedp(nums,n-1,dp)+0;
+    //     dp[n]=max(in,ex);
+    //     return dp[n];
+    // }
+    int tabulation(vector<int>&nums,int n)
     {
-        if(n<0)
+        vector<int>dp(n+1,0);
+        dp[0]=nums[0];
+        for(int i=1;i<=n;i++)
         {
-            return 0;
-        }
-        if(n==0)
-        {
-            return nums[0];
-        }
-        if(dp[n]!= -1)
-        {
-            return dp[n];
-        }
-        int in=solvedp(nums,n-2,dp)+nums[n];
-        int ex=solvedp(nums,n-1,dp)+0;
-        dp[n]=max(in,ex);
-        return dp[n];
+            
+            int temp=0;
+            if(i-2>=0)
+            {
+                temp=dp[i-2];
+            }
+            int in=temp+nums[i];
+            int ex=dp[i-1]+0;
+            dp[i]=max(in,ex);
+
+        }return dp[n];
     }
     int rob(vector<int>& nums) {
         int n=nums.size()-1;
-        vector<int>dp(n+1,-1);
-        return solvedp(nums,n,dp);
-
+    //     vector<int>dp(n+1,-1);
+    //     return solvedp(nums,n,dp);
+        return tabulation(nums,n);
+    
         
     }
 };

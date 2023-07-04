490571cff958a7f3be91bc06831cfbfb5831a9c7
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-07-04 12:15:58 +0530

    Time: 17 ms (9.06%) | Memory: 10.1 MB (20.94%) - LeetSync

---
+++ single-number-ii/single-number-ii.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int,int>mp;
+        for(auto it:nums)
+        {
+            mp[it]++;
+        }
+        for(auto it:mp)
+        {
+            if(it.second==1)
+            {
+                return it.first;
+            }
+        }
+        return -1;
+        
+    }
+};

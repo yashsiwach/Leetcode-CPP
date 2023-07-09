95acee61470963797ca66f69f49a0485249d6e7a
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-07-10 01:55:57 +0530

    Time: 106 ms (69.00%) | Memory: 14.5 MB (69.96%) - LeetSync

--- coin-change/coin-change.cpp
+++ coin-change/coin-change.cpp
@@ -1,34 +1,54 @@
 class Solution {
 public:
-    int solve(vector<int>&coins,int amount,vector<int>&jat)
+    // int solve(vector<int>&coins,int amount,vector<int>&jat)
+    // {
+    //     if(amount==0)
+    //     {
+    //         return 0;
+    //     }
+    //     if(amount<0)
+    //     {
+    //         return INT_MAX;
+    //     }
+    //     if(jat[amount]!=-1)
+    //     {
+    //         return jat[amount];
+    //     }
+    //     int mini=INT_MAX;
+    //     for(int i=0;i<coins.size();i++)
+    //     {
+    //         int ans=solve(coins,amount-coins[i],jat);
+    //         if(ans!=INT_MAX)
+    //         {
+    //             mini=min(1+ans,mini);
+    //         }
+    //     }
+    //     jat[amount]=mini;
+    //     return jat[amount];
+    // }
+    int solve(vector<int>&coins,int amount)
     {
-        if(amount==0)
+        vector<int>jat(amount+1,INT_MAX);
+        jat[0]=0;
+        for(int i=1;i<=amount;i++)
         {
-            return 0;
+            int mini=INT_MAX;
+            for(int j=0;j<coins.size();j++)
+        {       if(i-coins[j]>=0){
+                  int ans=jat[i-coins[j]];
+                  if(ans!=INT_MAX)
+                  {
+                      mini=min(1+ans,mini);
+                  }
         }
-        if(amount<0)
-        {
-            return INT_MAX;
         }
-        if(jat[amount]!=-1)
-        {
-            return jat[amount];
-        }
-        int mini=INT_MAX;
-        for(int i=0;i<coins.size();i++)
-        {
-            int ans=solve(coins,amount-coins[i],jat);
-            if(ans!=INT_MAX)
-            {
-                mini=min(1+ans,mini);
-            }
-        }
-        jat[amount]=mini;
-        return jat[amount];
+        jat[i]=mini;
+        }return jat[amount];
+
     }
     int coinChange(vector<int>& coins, int amount) {
-        vector<int>jat(amount+1,-1);
-        int ans= solve(coins,amount,jat);
+     
+        int ans= solve(coins,amount);
         if(ans==INT_MAX)
         {
             return -1;

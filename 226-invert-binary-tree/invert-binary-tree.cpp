97f4fb8143dfda4c8d660334f827037a4864974b
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-08-10 12:26:13 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (53.36%) - LeetSync

--- 226-invert-binary-tree/invert-binary-tree.cpp
+++ 226-invert-binary-tree/invert-binary-tree.cpp
@@ -1,41 +1,22 @@
-// class Solution {
-// public:
-//     void solve(TreeNode* left, TreeNode* right) {
-//         if (left == nullptr || right == nullptr) {
-//             return;
-//         }
-        
-//         // Swap the values of the nodes
-//         swap(left->val, right->val);
-        
-//         // Recursively invert the subtrees
-//         solve(left->left, right->right);
-//         solve(left->right, right->left);
-//     }
-    
-//     TreeNode* invertTree(TreeNode* root) {
-//         if (root == nullptr) {
-//             return nullptr;
-//         }
-        
-//         // Start the recursive inversion
-//         solve(root->left, root->right);
-        
-//         return root;
-//     }
-// };
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if (root == nullptr) {
-            return nullptr;
-        }
-        
-        // Swap the left and right subtrees
-        TreeNode* temp = root->left;
-        root->left = invertTree(root->right);
-        root->right = invertTree(temp);
-        
+         if(root==NULL)
+        return NULL ;
+        invertTree(root->left);
+        invertTree(root->right);
+        swap(root->left,root->right);
         return root;
     }
 };

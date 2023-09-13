9761cec8a569694ca2a365f01bd7384573c89fed
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2023-09-13 12:07:34 +0530

    Time: 55 ms (7.08%), Space: 17.7 MB (47.6%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        if (n == 1) return 1;
+
+        vector<int> v(n, 1);
+
+        
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                v[i] = v[i - 1] + 1;
+            }
+        }
+
+        
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                v[i] = max(v[i], v[i + 1] + 1);
+            }
+        }
+
+        int sum = 0;
+        for (int i : v) {
+            sum += i;
+            cout << i << " ";
+        }
+        return sum;
+    }
+};

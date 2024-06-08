b81a4055f796c021932d0ad739c6249b8bfa7753
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2024-06-09 00:26:42 +0530

    Time: 1026 ms (12.5%), Space: 46.7 MB (12.5%) - LeetHub

--- 3176-find-the-maximum-length-of-a-good-subsequence-i/3176-find-the-maximum-length-of-a-good-subsequence-i.cpp
+++ 3176-find-the-maximum-length-of-a-good-subsequence-i/3176-find-the-maximum-length-of-a-good-subsequence-i.cpp
@@ -1,26 +1,55 @@
-class Solution {
-public:
-int dp[26][503][503];
-    int solve(vector<int>& v, int k, int ind, int prev) {
-        if (ind >= v.size()) return 0;
 
-        int take = 0, skip = 0, maxi = 0;
-        if(dp[k][ind][prev]!=-1)return dp[k][ind][prev];
-        for (int i = ind; i < v.size(); i++) {
-            if (ind == 0 || v[i] == v[prev]) {
-                take = 1 + solve(v, k, i + 1,i); // Corrected: i + 1
-            } else {
-                if(k>0)skip = 1 + solve(v, k - 1, i + 1, i); // Corrected: i + 1
-            }
-            maxi = max(maxi, max(take, skip));
+#define ll long long
+int dp[503][503][26];
+
+
+int find(int pos, int prev, int cnt, vector<int> &nums)
+{
+    if(pos == nums.size())
+        return 0;
+    
+    int ans = -1; 
+
+    if(dp[pos][prev+1][cnt] != -1)
+        return dp[pos][prev+1][cnt];
+    
+    if(prev == -1) // prev = -1 implies the the subsequence has not started
+    {
+        ans = max(ans, find(pos+1, -1, cnt, nums)); // not starting
+        ans = max(ans, 1 + find(pos+1, pos, cnt, nums)); //starting
+    }
+    
+    else
+    {
+        if(nums[pos] == nums[prev]) // if prev == curr no need to reduce the cnt when adding to subsequence
+        {
+            ans = max(ans, find(pos+1, prev, cnt, nums)); // skipping
+            ans = max(ans, 1 + find(pos+1, pos, cnt, nums)); // adding
         }
         
-        return dp[k][ind][prev]=maxi;
+        else
+        {
+            ans = max(ans, find(pos+1, prev, cnt, nums)); // skipping
+            
+            if(cnt > 0) // make sure cnt > 0 before adding a number not equal to the previous. 
+                ans = max(ans, 1 + find(pos+1, pos, cnt-1, nums)); // adding and reducing cnt
+        }
+    
     }
+        
+    return dp[pos][prev+1][cnt] = ans;
+}
 
+
+class Solution {
+public:
     int maximumLength(vector<int>& nums, int k) {
-memset(dp,-1,sizeof(dp));
-        if (nums.empty()) return 0; // Added check for empty array
-        return solve(nums, k, 0, nums[0]); // Correct initial call
+        
+        memset(dp, -1, sizeof(dp));
+        
+        int ans = find(0, -1, k, nums);
+        return ans;
     }
 };
+
+

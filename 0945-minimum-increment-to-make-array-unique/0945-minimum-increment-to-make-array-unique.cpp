5eb63af83831a376cf0119f88d01d8cb18161a42
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2024-06-14 08:05:30 +0530

    Time: 122 ms (52.95%), Space: 69.1 MB (79.13%) - LeetHub

--- 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
+++ 0945-minimum-increment-to-make-array-unique/0945-minimum-increment-to-make-array-unique.cpp
@@ -3,18 +3,13 @@ public:
     int minIncrementForUnique(vector<int>& v) {
         sort(v.begin(),v.end());
         int count=0;
-        set<int>st{v[0]};
         for(int i=1;i<v.size();i++)
         {
-            if(st.find(v[i])!=st.end())
+            if(v[i]<=v[i-1])
             {
-                int maxi=*st.rbegin();
-                count+=(maxi+1)-v[i];
-                st.insert(maxi+1);
-                cout<<count<<" "<<v[i]<<endl;
+                count+=(v[i-1]+1)-v[i];
+                v[i]=v[i-1]+1;
             }
-            else st.insert(v[i]);
-
         }
         return count;
         

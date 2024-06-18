73d5a518ae098b0821441fd094c07beb112724e2
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2024-06-18 11:20:41 +0530

    Time: 70 ms (33.76%), Space: 41.8 MB (31.57%) - LeetHub

--- 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
+++ 0826-most-profit-assigning-work/0826-most-profit-assigning-work.cpp
@@ -1,35 +1,28 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& d, vector<int>& p, vector<int>& w) {
-        vector<pair<int, int>> v;
-        for(int i = 0; i < p.size(); i++) {
-            v.push_back({d[i], p[i]});
+        vector<pair<int,int>>v;
+        for(int i=0;i<p.size();i++)
+        {
+            v.push_back({d[i],p[i]});
         }
-
-        sort(v.begin(), v.end(), [](auto &a, auto &b) {
-            if(a.first != b.first) return a.first < b.first;
-            else return a.second < b.second;
-        });
-
-        vector<int> pref(p.size());
-        for(int i = 0; i < v.size(); i++) {
-            if(i == 0) pref[i] = v[i].second;
-            else pref[i] = max(pref[i - 1], v[i].second);
+        sort(v.begin(),v.end(),[](auto &a,auto &b){if(a.first!=b.first)return a.first<b.first;else return a.second<b.second;});
+        sort(d.begin(),d.end());
+        vector<int>pref(d.size());
+        for(int i=0;i<d.size();i++)
+        {
+            if(i==0)pref[i]=v[i].second;
+            else pref[i]=max(pref[i-1],v[i].second);
         }
-
-        int ans = 0;
-        for(auto &it : w) {
-            auto temp = upper_bound(v.begin(), v.end(), make_pair(it, INT_MAX), [](const auto &a, const auto &b) {
-                return a.first < b.first;
-            });
-
-            if (temp != v.begin()) {
-                --temp;
-                int ind = temp - v.begin();
-                ans += pref[ind];
+        int ans=0;
+        for(auto &it:w)
+        {
+            auto temp=upper_bound(d.begin(),d.end(),it);
+            if(temp!=d.begin()){--temp;
+            int ind=temp-d.begin();
+            ans+=pref[ind];
             }
         }
-
         return ans;
     }
 };

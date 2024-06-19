69d57e8d34ab73ca328332367e79be39e4db85ad
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2024-06-19 09:17:22 +0530

    Time: 137 ms (5.04%), Space: 69 MB (65.37%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,46 +1,38 @@
 class Solution {
 public:
-bool check(int mid,vector<int>&v,int m,int k)
-{
-    int temp=0,count=0;
-    for(int i=0;i<v.size();i++)
-    {
-        if(v[i]<=mid)
-        {
-            count++;
-        }
-        else
-        {
-            count=0;
-        }
-        if(count==k)
-        {
-            count=0;
-            temp++;
-        }
-    }
-    if(temp>=m)return true;
-    else return false;
-
-}
-    int minDays(vector<int>& b, int m, int k) {
-        int ans=0;
-        if(m*k>b.size())return -1;
-        int l=0,r=1e9;
-        while(l<=r)
-        {
-            int mid=l+(r-l)/2;
-            if(check(mid,b,m,k))
-            {
-                ans=mid;
-                r=mid-1;
+    bool check(int mid, vector<int>& v, int m, int k) {
+        int temp = 0, count = 0;
+        for(int i = 0; i < v.size(); i++) {
+            if(v[i] <= mid) {
+                count++;
+            } else {
+                count = 0;
             }
-            else
-            {
-                l=mid+1;
+            if(count == k) {
+                count = 0;
+                temp++;
+            }
+        }
+        return temp >= m;
+    }
+
+    int minDays(vector<int>& b, int m, int k) {
+        long long n = b.size();
+        long long required = (long long)m * k;  // Avoid overflow by using long long
+
+        // If it's impossible to make m bouquets
+        if(required > n) return -1;
+
+        int l = 0, r = 1e9, ans = 0;
+        while(l <= r) {
+            int mid = l + (r - l) / 2;
+            if(check(mid, b, m, k)) {
+                ans = mid;
+                r = mid - 1;
+            } else {
+                l = mid + 1;
             }
         }
         return ans;
-        
     }
 };

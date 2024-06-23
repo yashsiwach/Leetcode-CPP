d965a5c9dd45ea919dbedf3cb6e30ad16acac718
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2024-06-23 17:08:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int mincostTickets(vector<int>& d, vector<int>& c) {
+        long long n=d.size();
+        vector<vector<long long>>dp(366,vector<long long>(3,INT_MAX));
+        vector<bool>v(365,false);
+        for(long long i=0;i<n;i++)
+        {
+            v[d[i]-1]=true;
+        }
+        dp[d[0]][0]=c[0];
+         dp[d[0]][1]=c[1];
+          dp[d[0]][2]=c[2];
+        for(long long i=d[0];i<=365;i++)
+        {
+            if(i==d[0])continue;
+            if(v[i-1]==true)dp[i][0]=min({dp[i-1][0],dp[i-1][1],dp[i-1][2]})+c[0];
+            else dp[i][0]=dp[i-1][0];
+
+            if(i-7>0&&v[i-1]==true){
+                dp[i][1]=min({dp[i-7][0],dp[i-7][1],dp[i-7][2]})+c[1];
+            }
+            else dp[i][1]=dp[i-1][1];
+
+            if(i-30>0&&v[i-1]==true){
+                dp[i][2]=min({dp[i-30][0],dp[i-30][1],dp[i-30][2]})+c[2];
+            }
+            else dp[i][2]=dp[i-1][2];
+            
+        }
+        return min({dp[365][0],dp[365][1],dp[365][2]});
+
+        
+    }
+};

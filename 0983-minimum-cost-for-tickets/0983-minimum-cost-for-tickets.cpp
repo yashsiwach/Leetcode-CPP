cb30a724d566aa471ccf0af0304d5efff259aec5
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2024-06-23 17:10:06 +0530

    Time: 3 ms (58.08%), Space: 12.4 MB (26.69%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.cpp
@@ -1,36 +1,20 @@
 class Solution {
 public:
-    int mincostTickets(vector<int>& d, vector<int>& c) {
-        long long n=d.size();
-        if(d.back()==28&&c.back()==31)return 23;
-        vector<vector<long long>>dp(366,vector<long long>(3,INT_MAX));
-        vector<bool>v(365,false);
-        for(long long i=0;i<n;i++)
-        {
-            v[d[i]-1]=true;
-        }
-        dp[d[0]][0]=c[0];
-         dp[d[0]][1]=c[1];
-          dp[d[0]][2]=c[2];
-        for(long long i=d[0];i<=365;i++)
-        {
-            if(i==d[0])continue;
-            if(v[i-1]==true)dp[i][0]=min({dp[i-1][0],dp[i-1][1],dp[i-1][2]})+c[0];
-            else dp[i][0]=dp[i-1][0];
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+        vector<int> dp(366, 0);  // dp[i] will be the minimum cost to cover travel up to day i
+        unordered_set<int> travelDays(days.begin(), days.end());
 
-            if(i-7>0&&v[i-1]==true){
-                dp[i][1]=min({dp[i-7][0],dp[i-7][1],dp[i-7][2]})+c[1];
+        for (int i = 1; i <= 365; ++i) {
+            if (travelDays.count(i) == 0) {
+                dp[i] = dp[i-1];
+            } else {
+                int cost1 = dp[i-1] + costs[0];  // 1-day pass
+                int cost7 = dp[max(0, i-7)] + costs[1];  // 7-day pass
+                int cost30 = dp[max(0, i-30)] + costs[2];  // 30-day pass
+                dp[i] = min({cost1, cost7, cost30});
             }
-            else dp[i][1]=dp[i-1][1];
-
-            if(i-30>0&&v[i-1]==true){
-                dp[i][2]=min({dp[i-30][0],dp[i-30][1],dp[i-30][2]})+c[2];
-            }
-            else dp[i][2]=dp[i-1][2];
-            
         }
-        return min({dp[365][0],dp[365][1],dp[365][2]});
 
-        
+        return dp[365];
     }
 };

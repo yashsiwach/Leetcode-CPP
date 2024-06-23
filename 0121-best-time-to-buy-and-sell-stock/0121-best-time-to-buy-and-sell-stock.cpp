a5ea0c7d9d69e9559bc5ffd63ffece8d1288e0f1
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2024-06-23 14:51:59 +0530

    Time: 203 ms (5.18%), Space: 156.1 MB (5.08%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        int i =0,j=n-1;
-        int min = prices[0];
-        int profit = 0;
-        for(int i=1;i<n;i++){
-            if(prices[i]>min){
-               profit = max(profit,prices[i]-min);
-            }else if (prices[i]<min){
-                min = prices[i];
-            }else{
-                continue;
-            }
+    int maxProfit(vector<int>& v) {
+        int n=v.size();
+        vector<vector<int>>dp(n+1,vector<int>(2,0));
+        for(int i=0;i<=n;i++)
+        {
+            dp[i][0]=INT_MAX;
         }
-        cout<<min;
-        return profit;
+        dp[1][0]=v[0];
+        for(int i=2;i<=n;i++)
+        {
+            dp[i][0]=min(dp[i-1][0],v[i-1]);
+            dp[i][1]=max(dp[i-1][1],v[i-1]-dp[i-1][0]);
+           // cout<<dp[i][0]<<" "<<dp[i][1]<<endl;
+        }
+        return dp[n][1];
     }
 };

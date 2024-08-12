eebca5f59685f2436c6d40481e58476647de1732
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2024-08-12 22:39:14 +0530

    Time: 258 ms (5.77%), Space: 96.2 MB (5.09%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
-    int solve(vector<int>& coins, int amount,vector<int>&dp) {
-        if (amount < 0) {
-            return -1; // Return -1 to indicate it's not possible to make change.
-        }
-        if (amount == 0) {
+    int minCoins(vector<int>& coins,int amount,int ind,int n,vector<vector<int>>& dp) {
+        if(amount < 0 || ind == n) {
+            return -1;
+        }else if(amount == 0) {
             return 0;
         }
-        if(dp[amount]!=INT_MIN)
-        {
-            return dp[amount];
+        if(dp[amount][ind]!=INT_MAX)return dp[amount][ind];
+        if(coins[ind] <= amount) {
+            int take = minCoins(coins,amount - coins[ind],ind,n,dp);
+            int notTake = minCoins(coins,amount,ind + 1,n,dp);
+            if(take == -1 && notTake == -1)return dp[amount][ind]= -1;
+            else if(take != -1 && notTake != -1){
+                return dp[amount][ind]=min(1+take,notTake);
+            }else if(notTake != -1)return dp[amount][ind]=notTake;
+            else if(take != -1)return dp[amount][ind]=1 + take;
+        }else {
+            return  dp[amount][ind]=minCoins(coins,amount,ind + 1,n,dp);
         }
-        int ans = INT_MAX;
-        for (int i = 0; i < coins.size(); i++) {
-            int subproblem = solve(coins, amount - coins[i],dp);
-            if (subproblem == -1) {
-                continue; 
-            }
-           ans = min(ans, 1 + subproblem);
-        }
-        return  dp[amount]=(ans == INT_MAX) ? -1 : ans;
+        return dp[amount][ind]=-1;
     }
-
     int coinChange(vector<int>& coins, int amount) {
-        vector<int>dp(amount+1,INT_MIN);
-        return solve(coins, amount,dp);
+        vector<vector<int>> dp(amount+1, vector<int> (coins.size() + 1,INT_MAX));
+       // vector<int> dp(coins.size(),-1);
+        int ans = minCoins(coins,amount,0,coins.size(),dp);
+        return ans;
     }
 };

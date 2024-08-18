7eb7ca4bfd927e76ffd3eae2bf914007c5fa5e01
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2024-08-18 16:21:08 +0530

    Time: 0 ms (100%), Space: 9.7 MB (72.38%) - LeetHub

--- 0264-ugly-number-ii/0264-ugly-number-ii.cpp
+++ 0264-ugly-number-ii/0264-ugly-number-ii.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int nthUglyNumber(int n) {
-        std::set<long> v = {1};
-        auto it = v.begin(); // Iterator to the set
-int count=0;
-        while ( count< n) {
-            v.insert(*it * 2);
-            v.insert(*it * 3);
-            v.insert(*it * 5);
-            count++;
-            ++it;
+        vector<int> ans(n + 1, 0);
+        ans[1] = 1;  // The first ugly number is 1.
+
+        int i2 = 1, i3 = 1, i5 = 1;
+        for (int i = 2; i <= n; i++) {
+            long long next2 = ans[i2] * 2;
+            long long next3 = ans[i3] * 3;
+            long long next5 = ans[i5] * 5;
+            
+            ans[i] = min(next2, min(next3, next5));
+
+            // Advance the indices for whichever factor was used
+            if (ans[i] == next2) i2++;
+            if (ans[i] == next3) i3++;
+            if (ans[i] == next5) i5++;
         }
 
-        // Reset the iterator to the beginning
-        it = v.begin();
-        // Advance the iterator to the nth element
-        std::advance(it, n - 1);
-        for(auto it:v)cout<<it<<" ";
-        return *it;
+        return ans[n];
     }
 };

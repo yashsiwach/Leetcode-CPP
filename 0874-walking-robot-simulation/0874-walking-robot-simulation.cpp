e99cb6b375897288bb959a3fa5b3266f79355864
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2024-09-04 14:52:46 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int robotSim(vector<int>& v, vector<vector<int>>& o) {
+        set<pair<int, int>> st;
+        for(auto &it:o)
+        {
+            st.insert({it[0],it[1]});
+        }
+        int curr = 0;
+        set<pair<int,int>>final;
+        pair<int, int> point = {0, 0};
+        for (auto& it : v) {
+            curr=curr%4;
+            if (it == -1) {
+                curr++;
+                continue;
+            } else if (it == -2) {
+                curr--;
+                continue;
+            }
+            if (curr == 0) {
+                while (it--) {
+                    point.second++;
+                    if (st.find(point) != st.end()) {
+                        point.second--;
+                        break;
+                    }
+                }
+            }
+            if (curr == 1) {
+                while (it--) {
+                    point.first++;
+                    if (st.find(point) != st.end()) {
+                        point.first--;
+                        break;
+                    }
+                }
+            }
+            if (curr == 2) {
+                while (it--) {
+                    point.second--;
+                    if (st.find(point) != st.end()) {
+                        point.second++;
+                        break;
+                    }
+                }
+            }
+            if (curr == 3) {
+                while (it--) {
+                    point.first--;
+                    if (st.find(point) != st.end()) {
+                        point.first++;
+                        break;
+                    }
+                }
+            }
+            final.insert(point);
+           // cout<<point.first<<" "<<point.second<<endl;
+        }
+        point=*final.rbegin();
+        return (point.first*point.first)+(point.second*point.second);
+    }
+};

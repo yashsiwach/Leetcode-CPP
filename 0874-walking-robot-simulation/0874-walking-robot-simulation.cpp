93316e7e43df74af341cef6442e1769085893a95
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2024-09-04 14:59:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
+++ 0874-walking-robot-simulation/0874-walking-robot-simulation.cpp
@@ -2,37 +2,62 @@ class Solution {
 public:
     int robotSim(vector<int>& v, vector<vector<int>>& o) {
         set<pair<int, int>> st;
-        for(auto &it : o) {
-            st.insert({it[0], it[1]});
+        for(auto &it:o)
+        {
+            st.insert({it[0],it[1]});
         }
-        int curr = 0;  // 0 -> North, 1 -> East, 2 -> South, 3 -> West
+        int curr = 0;
+        set<pair<int,int>>final;
         pair<int, int> point = {0, 0};
-        int maxDist = 0;
-        
         for (auto& it : v) {
+            
             if (it == -1) {
-                curr = (curr + 1) % 4;  // Right turn
+                curr=(curr+1)%4;
+                continue;
             } else if (it == -2) {
-                curr = (curr - 1 + 4) % 4;  // Left turn, ensuring curr is non-negative
-            } else {
-                for (int i = 0; i < it; ++i) {
-                    if (curr == 0) point.second++;  // Move North
-                    if (curr == 1) point.first++;   // Move East
-                    if (curr == 2) point.second--;  // Move South
-                    if (curr == 3) point.first--;   // Move West
-                    
-                    if (st.find(point) != st.end()) {  // Check for obstacles
-                        if (curr == 0) point.second--;
-                        if (curr == 1) point.first--;
-                        if (curr == 2) point.second++;
-                        if (curr == 3) point.first++;
+                curr=(curr-1+4)%4;
+                continue;
+            }
+            if (curr == 0) {
+                while (it--) {
+                    point.second++;
+                    if (st.find(point) != st.end()) {
+                        point.second--;
                         break;
                     }
                 }
             }
-            maxDist = max(maxDist, point.first * point.first + point.second * point.second);
+            if (curr == 1) {
+                while (it--) {
+                    point.first++;
+                    if (st.find(point) != st.end()) {
+                        point.first--;
+                        break;
+                    }
+                }
+            }
+            if (curr == 2) {
+                while (it--) {
+                    point.second--;
+                    if (st.find(point) != st.end()) {
+                        point.second++;
+                        break;
+                    }
+                }
+            }
+            if (curr == 3) {
+                while (it--) {
+                    point.first--;
+                    if (st.find(point) != st.end()) {
+                        point.first++;
+                        break;
+                    }
+                }
+            }
+            final.insert(point);
+           // cout<<point.first<<" "<<point.second<<endl;
         }
-        
-        return maxDist;
+        point=*final.rbegin();
+        return (point.first*point.first)+(point.second*point.second);
     }
 };

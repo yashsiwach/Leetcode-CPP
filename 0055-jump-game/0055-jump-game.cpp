d6fbdec121d104242fe9fb1accbeb3cadacc8445
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2024-09-11 21:02:25 +0530

    Time: 710 ms (5.55%), Space: 54.4 MB (6.11%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
-    bool solve(vector<int>& nums, int ind, vector<int>& dp) {
-        if (ind >= nums.size() - 1) {
-            return true;  // If we reached or surpassed the last index, return true
-        }
-
-        if (dp[ind] != -1) {
-            return dp[ind];  // Return cached result
-        }
-
-        for (int i = ind + 1; i <= ind + nums[ind] && i < nums.size(); i++) {
-            if (solve(nums, i, dp)) {
-                return dp[ind] = true;  // Memoize the result as true
-            }
-        }
-
-        return dp[ind] = false;  // Memoize the result as false
+bool solve(vector<int>&nums,int ind,vector<int>&dp)
+{
+    if(ind>=nums.size()-1)return  true;
+    if(dp[ind]!=-1)return dp[ind];
+    for(int i=ind+1;i<=ind+nums[ind]&&i<nums.size();i++)
+    {
+     
+        if(solve(nums,i,dp))return true;
     }
-
+    return dp[ind]=false;
+}
     bool canJump(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1);  // Memoization table, initialized to -1 (unknown)
-        return solve(nums, 0, dp);
+        vector<int>dp(nums.size()+1,-1);
+        return solve(nums,0,dp);
+        
     }
 };

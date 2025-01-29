50e9c15757bf092654b817ac70ace048d2e1cf43
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2025-01-29 12:36:36 +0530

    Time: 5 ms (46.33%), Space: 9.2 MB (66.34%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,25 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    bool rec(string a, string b, string c, int i, int j, int k) {
-        if(k == c.size()) {
-            return i == a.size() && j == b.size();
-        }
-        
-        bool e = false;
-        bool f = false;
-        
-        if(i < a.size() && a[i] == c[k]) {
-            e = rec(a, b, c, i + 1, j, k + 1);
-        }
-        
-        if(j < b.size() && b[j] == c[k]) {
-            f = rec(a, b, c, i, j + 1, k + 1);
-        }
-        
-        return e || f;
-    }
-    
     bool isInterleave(string a, string b, string c) {
-        return rec(a, b, c, 0, 0, 0);
+        if(a.size() + b.size() != c.size()) return false;
+        
+        vector<vector<bool>> dp(a.size() + 1, vector<bool>(b.size() + 1, false));
+
+        dp[0][0] = true;
+
+        for(int i = 1; i <= a.size(); i++) {
+            dp[i][0] = dp[i - 1][0] && (a[i - 1] == c[i - 1]);
+        }
+
+        for(int j = 1; j <= b.size(); j++) {
+            dp[0][j] = dp[0][j - 1] && (b[j - 1] == c[j - 1]);
+        }
+
+        for(int i = 1; i <= a.size(); i++) {
+            for(int j = 1; j <= b.size(); j++) {
+                int k = i + j - 1;
+                if(a[i - 1] == c[k]) {
+                    dp[i][j] = dp[i][j] || dp[i - 1][j];
+                }
+                if(b[j - 1] == c[k]) {
+                    dp[i][j] = dp[i][j] || dp[i][j - 1];
+                }
+            }
+        }
+
+        return dp[a.size()][b.size()];
     }
 };

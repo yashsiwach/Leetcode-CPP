345bf3bb99397dab85b0a1f021215b2b423a14a4
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2025-01-29 19:44:52 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -15,7 +15,24 @@ int rec(int i,int j,string &s,string &t)
     return dp[i][j]=ans;
 }
     int numDistinct(string s, string t) {
-        memset(dp,-1,sizeof(dp));
-        return rec(0,0,s,t);
+        // memset(dp,-1,sizeof(dp));
+        // return rec(0,0,s,t);
+    
+        long long n=s.size();
+        long long m=t.size();
+        if (m > n) return 0; 
+        vector<vector<long long>>dp(n+1,vector<long long>(m+1,0));
+        for (long long i = 0; i <= n; i++) dp[i][0] = 1; 
+        for(long long i=1;i<=n;i++)
+        {
+            for(long long j=1;j<=m;j++)
+            {
+                dp[i][j]=dp[i-1][j];
+                if(s[i-1]==t[j-1])dp[i][j]+=dp[i-1][j-1];
+
+            }
+        }
+        return (int)dp[n][m];
     }
+
 };

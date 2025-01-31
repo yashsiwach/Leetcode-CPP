95c82c5a93b82d8fc928468277080534444b22c2
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2025-01-31 13:53:20 +0530

    Time: 95 ms (42.17%), Space: 25.6 MB (44.1%) - LeetHub

--- 1048-longest-string-chain/1048-longest-string-chain.cpp
+++ 1048-longest-string-chain/1048-longest-string-chain.cpp
@@ -1,27 +1,23 @@
 class Solution {
 public:
-    unordered_map<string, int> mp;
-
-    int rec(unordered_set<string>& wordsSet, string word) {
-        if (mp.find(word) != mp.end()) return mp[word];
-
-        int maxLen = 1;
-        for (int i = 0; i < word.size(); i++) {
-            string pred = word.substr(0, i) + word.substr(i + 1);
-            if (wordsSet.count(pred)) {
-                maxLen = max(maxLen, 1 + rec(wordsSet, pred));
+    map<string,int>mp;
+    int rec(set<string>&se,string word){
+        if(mp.find(word)!=mp.end())return mp[word];
+        int ans=1;
+        for(int i=0;i<word.size();i++){
+            string pred=word.substr(0,i)+word.substr(i+1);
+            if(se.count(pred)){
+                ans=max(ans,1+rec(se,pred));
             }
         }
-        return mp[word] = maxLen;
+        return mp[word]=ans;
     }
-
-    int longestStrChain(vector<string>& words) {
-        unordered_set<string> wordsSet(words.begin(), words.end());
-        int maxChain = 0;
-
-        for (const string& word : words) {
-            maxChain = max(maxChain, rec(wordsSet, word));
+    int longestStrChain(vector<string>&words){
+        set<string>se(words.begin(),words.end());
+        int ans=0;
+        for(const string&word:words){
+            ans=max(ans,rec(se,word));
         }
-        return maxChain;
+        return ans;
     }
 };

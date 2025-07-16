044017fb1b41f4ff558f2548e5ff559226bdae9d
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2025-07-16 10:35:59 +0530

    Time: 0 ms (100%), Space: 16.6 MB (97.37%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,54 +1,46 @@
-#include <vector>
-#include <queue>
-#include <algorithm>
-
-using namespace std;
-
 class Solution {
 public:
+
     int orangesRotting(vector<vector<int>>& v) {
-        queue<pair<int, pair<int, int>>> q;
-        int maxi = 0;
-        int n = v.size();
-        int m = v[0].size(); // Number of columns
-        
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (v[i][j] == 2) {
-                    q.push({i, {j, 0}});
+        int n=v.size();
+        int m=v[0].size();
+        vector<int>x={0,-1,0,1};
+        vector<int>y={-1,0,1,0};
+        queue<pair<int,int>>q;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(v[i][j]==2){
+                    q.push({i,j});
                 }
             }
         }
-
-        vector<int> a = {-1, 0, 1, 0};
-        vector<int> b = {0, 1, 0, -1};
-
-        while (!q.empty()) {
-            pair<int, pair<int, int>> temp = q.front();
-            q.pop();
-            int i = temp.first;
-            int j = temp.second.first;
-            int num = temp.second.second;
-            for (int k = 0; k < 4; k++) { // Use a different variable name here
-                int row = i + a[k]; // Corrected indexing
-                int col = j + b[k];
-                if (row >= 0 && row < n && col >= 0 && col < m && v[row][col] == 1) {
-                    v[row][col] = 2;
-                    q.push({row, {col, num + 1}});
-                    maxi = max(num + 1, maxi);
+        int ans=0;
+        while(!q.empty()){
+            int sz=q.size();
+            bool ok = false;
+            for(int i=0;i<sz;i++){
+                auto f=q.front();
+                q.pop();
+                int cx=f.first;
+                int cy=f.second;
+                for(int i=0;i<4;i++){
+                    int nx=cx+x[i];
+                    int ny=cy+y[i];
+                    if(nx<0 || ny<0 || nx>=n || ny>=m || v[nx][ny]!=1) continue;
+                    v[nx][ny]=2;
+                    q.push({nx,ny});
+                    ok = true;
                 }
             }
+            if(ok) ans++;
         }
-
-        // Check if any fresh orange is left
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (v[i][j] == 1) {
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(v[i][j]==1){
                     return -1;
                 }
             }
         }
-
-        return maxi;
+        return ans;
     }
 };

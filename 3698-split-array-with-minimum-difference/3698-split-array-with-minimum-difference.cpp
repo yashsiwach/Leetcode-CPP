0c7cc13f392c78640be3d3173ddf20648622366d
Yash Siwach <112823140+yashsiwach@users.noreply.github.com>
2025-09-30 16:51:01 +0530

    Time: 36 ms (6.5%), Space: 223.1 MB (5.32%) - LeetHub

--- 3698-split-array-with-minimum-difference/3698-split-array-with-minimum-difference.cpp
+++ 3698-split-array-with-minimum-difference/3698-split-array-with-minimum-difference.cpp
@@ -1,37 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution {
 public:
-    long long splitArray(vector<int>& v) {
-        long long n=v.size();
-        vector<pair<long long,bool>>left(n),right(n);
-        for(long long i=0;i<n;i++){
-            if(i==0){
-                left[i].first=v[0];
-                left[i].second=true;
-            }
-            else{
-                left[i].first=left[i-1].first+v[i];
-                left[i].second=(left[i-1].first<left[i].first?true:false);
-            }
-        }
-        for(long long i=n-1;i>=0;i--){
-            if(i==n-1){
-                right[i].first=v[i];
-                right[i].second=true;
-            }
-            else{
-                right[i].first=right[i+1].first+v[i];
-                right[i].second=(right[i]>right[i+1]?true:false);
+    long long splitArray(vector<int>& nums) {
+        long long n=nums.size();
+        vector<pair<long long,bool>> left(n), right(n);
+
+        // build prefix (strictly increasing check)
+        for (long long i=0;i<n;i++) {
+            if (i==0) {
+                left[i].first = nums[0];
+                left[i].second = true;
+            } else {
+                left[i].first = left[i-1].first + nums[i];
+                left[i].second = (left[i-1].second && nums[i-1] < nums[i]);
             }
         }
 
-        long long mini=INT_MAX;
-        for(int i=0;i<n-1;i++){
-            long long l=i,r=i+1;
-            if(left[l].second==true&&right[r].second==true){
-                long long cost=abs(left[l].first-right[r].first);
-                mini=min(mini,cost);
+        // build suffix (strictly decreasing check)
+        for (long long i=n-1;i>=0;i--) {
+            if (i==n-1) {
+                right[i].first = nums[i];
+                right[i].second = true;
+            } else {
+                right[i].first = right[i+1].first + nums[i];
+                right[i].second = (right[i+1].second && nums[i] > nums[i+1]);
             }
         }
-        return mini==0?-1:mini;
+
+        long long ans = LLONG_MAX;
+        bool ok = false;
+
+        // try all split points
+        for (int i=0;i<n-1;i++) {
+            if (left[i].second && right[i+1].second) {
+                long long diff = abs(left[i].first - right[i+1].first);
+                ans = min(ans, diff);
+                ok = true;
+            }
+        }
+
+        return ok ? ans : -1;
     }
 };
